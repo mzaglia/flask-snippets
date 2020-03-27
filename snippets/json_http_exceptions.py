e54c9ad15cc287b68eef89502fde861bf164f167
Matheus C. Zaglia <mzaglia@gmail.com>
2020-03-27 15:59:05 -0300

    Update json_http_exceptions.py

--- snippets/json_http_exceptions.py
+++ snippets/json_http_exceptions.py
@@ -3,9 +3,13 @@ from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-@app.errorhandler(HTTPException)
-def http_exception_handler(e):
-    return {'code': e.code, 'description': e.description}, e.code
+@app.errorhandler(Exception)
+def exception_handler(e):
+    """Handle Exceptions."""
+    if isinstance(e, HTTPException):
+        return {'code': e.code, 'description': e.description}, e.code
+    app.logger.exception(e)
+    return {'code': '500', 'description': 'Internal Server Error'}, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
